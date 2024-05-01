f74a5dd9c51b7834da500aae47fe71328a342892
Koray Temizkan <160160220+korayTemizkan@users.noreply.github.com>
2024-05-02 01:04:19 +0300

    7.gün yüklemesi
    İletişim formu htm,css ve js olmak üzere yeniden yazıldı .

--- WebTeknolojileriProje/iletisim.js
+++ WebTeknolojileriProje/iletisim.js
@@ -1,13 +1,176 @@
-function Start() {
-    let count = 0;
-    let veri = document.querySelectorAll(".veri");
-
-
-    veri.forEach((veriler) => {
-
-        if (veriler.value == "") {
-            veriler.style.color = "red";
-            veriler.value = "Eksik giriş!";
-        }
-    })
+function printError(Id, Msg) {
+    document.getElementById(Id).innerHTML = Msg;
 }
+
+function validateForm() {
+
+    var name = document.Form.name.value;
+    var email = document.Form.email.value;
+    var mobile = document.Form.mobile.value;
+    var country = document.Form.country.value;
+    var gender = document.Form.gender.value;
+    var password = document.Form.password.value;
+    var age = document.Form.age.value;
+    var color = document.Form.color.value;
+    var birthdate = document.Form.birthdate.value;
+    var subscribe = document.Form.subscribe.value;
+    var photo = document.Form.photo.value;
+    var textarea = document.Form.textarea.value;
+
+
+    var nameErr = emailErr = mobileErr = countryErr = genderErr = passwordErr = ageErr = colorErr = birthdateErr = subscribeErr = photoErr = textareaErr = true;
+
+
+    if (name == "") {
+        printError("nameErr", "Please enter your name");
+        var elem = document.getElementById("name");
+        elem.classList.add("input-2");
+        elem.classList.remove("input-1");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;
+        if (regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid name");
+            var elem = document.getElementById("name");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+            var elem = document.getElementById("name");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+        }
+    }
+
+
+    if (email == "") {
+        printError("emailErr", "Please enter your email address");
+        var elem = document.getElementById("email");
+        elem.classList.add("input-2");
+        elem.classList.remove("input-1");
+    } else {
+
+        var regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+            var elem = document.getElementById("email");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else {
+            printError("emailErr", "");
+            emailErr = false;
+            var elem = document.getElementById("email");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+
+        }
+    }
+
+
+    if (mobile == "") {
+        printError("mobileErr", "Please enter your mobile number");
+        var elem = document.getElementById("mobile");
+        elem.classList.add("input-2");
+        elem.classList.remove("input-1");
+    } else {
+        var regex = /^[1-9]\d{9}$/;
+        if (regex.test(mobile) === false) {
+            printError("mobileErr", "Please enter a valid 10 digit mobile number");
+            var elem = document.getElementById("mobile");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else {
+            printError("mobileErr", "");
+            mobileErr = false;
+            var elem = document.getElementById("mobile");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+        }
+    }
+
+
+    if (country == "Seçiniz") {
+        printError("countryErr", "Please select your country");
+        var elem = document.getElementById("country");
+        elem.classList.add("input-4");
+        elem.classList.remove("input-3");
+    } else {
+        printError("countryErr", "");
+        countryErr = false;
+        var elem = document.getElementById("country");
+        elem.classList.add("input-3");
+        elem.classList.remove("input-4");
+    }
+
+    if (gender == "") {
+        printError("genderErr", "Please select your gender");
+        var elem = document.getElementById("gender");
+        elem.classList.add("input-4");
+        elem.classList.remove("input-3");
+    } else {
+        printError("genderErr", "");
+        genderErr = false;
+        var elem = document.getElementById("gender");
+        elem.classList.add("input-3");
+        elem.classList.remove("input-4");
+    }
+
+    if (password == "") {
+        printError("passwordErr", "Please enter your password");
+        var elem = document.getElementById("password");
+        elem.classList.add("input-4");
+        elem.classList.remove("input-3");
+    } else {
+        printError("passwordErr", "");
+        elem.classList.add("input-3");
+        elem.classList.remove("input-4");
+    }
+
+    if (age == "") {
+        printError("ageErr", "Please enter your age");
+        var elem = document.getElementById("age");
+        elem.classList.add("input-4");
+        elem.classList.remove("input-3");
+    } else {
+
+        if (age < 15 || age > 100) {
+            printError("ageErr", "Please enter a number between 15-100");
+            var elem = document.getElementById("age");
+            elem.classList.add("input-4");
+            elem.classList.remove("input-3");
+        }
+        else {
+            printError("ageErr", "");
+            elem.classList.add("input-3");
+            elem.classList.remove("input-4");
+        }
+    }
+
+    if (birthdate === "") {
+        printError("birthdateErr", "Please enter your birthdate");
+        var elem = document.getElementById("birthdate");
+        elem.classList.add("input-4");
+        elem.classList.remove("input-3");
+    } else {
+        printError("birthdateErr", "");
+        var elem = document.getElementById("birthdate");
+        elem.classList.add("input-3");
+        elem.classList.remove("input-4");
+    }
+
+    if (!photo) {
+        printError("photoErr", "Please enter your photo");
+        var elem = document.getElementById("photo");
+        elem.classList.add("input-4");
+        elem.classList.remove("input-3");
+    } else {
+        printError("photoErr", "");
+        var elem = document.getElementById("photo");
+        elem.classList.add("input-3");
+        elem.classList.remove("input-4");
+    }
+
+    if ((nameErr || emailErr || mobileErr || countryErr || genderErr || passwordErr || ageErr || colorErr || birthdateErr || subscribeErr || photoErr || textareaErr) == true) {
+        return false;
+    }
+};
